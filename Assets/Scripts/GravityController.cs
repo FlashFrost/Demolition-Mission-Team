a13ef5c3b3b3c9187eafc4a8bb8b8fd58731099c
micoxion <mightygamermic@gmail.com>
2020-10-11 13:31:27 -0500

    basic gravity prototype working yahoo

---
+++ Assets/Scripts/GravityController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityController : MonoBehaviour
+{
+    public float pullPower;
+
+    private bool projectileInside = false;
+    private GameObject gravityWell;
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("Gravity Well")) {
+            Debug.Log("ELLO?");
+            projectileInside = true;
+            gravityWell = other.gameObject;
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if (other.gameObject.CompareTag("Gravity Well")) {
+            Debug.Log("GOBYE?");
+            projectileInside = false;
+            gravityWell = null;
+        }
+    }
+
+    private void FixedUpdate() {
+        if (projectileInside && gravityWell != null) {
+            gameObject.transform.LookAt(gravityWell.transform);
+            gameObject.GetComponent<Rigidbody>().AddForce(gameObject.transform.forward * pullPower);
+        }
+    }
+}
