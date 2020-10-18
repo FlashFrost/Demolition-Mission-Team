589f13d35d4709cebeefbafcaed6a467013d1f1a
FlashFrost <54652928+FlashFrost@users.noreply.github.com>
2020-10-18 14:54:20 -0500

    Modify Main menu

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -9,12 +9,15 @@ public class MainMenuController : MonoBehaviour
     [Header("Set In Inspector")]
     public GameObject menuVoidbox;
     public GameObject levelSelectVoidbox;
+    public GameObject levelLoadingVoidbox;
+    public TextMeshProUGUI levelLoadingtext;
 
     // Start is called before the first frame update
     void Start()
     {
         menuVoidbox.SetActive(true);
         levelSelectVoidbox.SetActive(false);
+        levelLoadingVoidbox.SetActive(false);
     }
 
     public void playGame()
@@ -30,23 +33,61 @@ public class MainMenuController : MonoBehaviour
 
     public void Level1()
     {
-        PlayerPrefs.SetInt("Level", 1);
-        SceneManager.LoadScene(1);
-        //bool debugBool = PlayerPrefs.HasKey("Level");
-        //Debug.Log(debugBool);
-        //int debugInt = PlayerPrefs.GetInt("Level");
-        //Debug.Log(debugInt);
-    }   
+        NowLoading(1);
+    }
     public void Level2()
     {
-        PlayerPrefs.SetInt("Level", 2);
-        SceneManager.LoadScene(1);
-
+        NowLoading(2);
     }
     public void Level3()
     {
-        PlayerPrefs.SetInt("Level", 3);
-        SceneManager.LoadScene(1);
+        NowLoading(3);
+    }
+    public void Level4()
+    {
+        NowLoading(4);
+    }
+    public void Level5()
+    {
+        NowLoading(5);
+    }
+    public void Level6()
+    {
+        NowLoading(6);
+    }
+    public void Level7()
+    {
+        NowLoading(7);
+    }
+    public void Level8()
+    {
+        NowLoading(8);
+    }
+    public void Level9()
+    {
+        NowLoading(9);
+    }
+    public void Level10()
+    {
+        NowLoading(10);
+    }
+    public void Level11()
+    {
+        NowLoading(11);
+    }
+    public void Level12()
+    {
+        NowLoading(12);
+    }
 
+    private void NowLoading(int levelCall)
+    {
+        levelSelectVoidbox.SetActive(false);
+        levelLoadingtext.text = ("Now Loading Level: " + levelCall);
+        levelLoadingVoidbox.SetActive(true);
+
+        Debug.Log(levelCall);
+        //PlayerPrefs.SetInt("Level", levelCall);
+        //SceneManager.LoadScene(1);
     }
 }
